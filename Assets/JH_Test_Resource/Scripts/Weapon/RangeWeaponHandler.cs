ad70df347be4668c48216cfa80ade0c6f39606c8
kimjinhong2023 <kimjinhong01@naver.com>
2025-02-25 00:19:18 +0900

    Feat: 몬스터 변경 테스트
    몬스터 스프라이트 변경, 애니메이션 적용 테스트

--- Assets/JH_Test_Resource/Scripts/Weapon/RangeWeaponHandler.cs
+++ Assets/JH_Test_Resource/Scripts/Weapon/RangeWeaponHandler.cs
@@ -28,6 +28,13 @@ public class RangeWeaponHandler : WeaponHandler
     [SerializeField] private Color projectileColor;
     public Color ProjectileColor { get { return projectileColor; } }
 
+    private ProjectileManager projectileManager;
+    protected override void Start()
+    {
+        base.Start();
+        projectileManager = ProjectileManager.Instance;
+    }
+
     public override void Attack()
     {
         base.Attack();
@@ -48,7 +55,10 @@ public class RangeWeaponHandler : WeaponHandler
 
     private void CreateProjectile(Vector2 _lookDirection, float angle)
     {
-
+        projectileManager.ShootBullet(
+            this,
+            projectileSpawnPosition.position,
+            RotateVector2(_lookDirection, angle));
     }
 
     private static Vector2 RotateVector2(Vector2 v, float degree)
