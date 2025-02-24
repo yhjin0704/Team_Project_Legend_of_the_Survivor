114c0e151d682447cedd94e20320477ea02b537b
yhjin0704 <yhjin07@naver.com>
2025-02-24 16:31:23 +0900

    <wip>

--- Assets/Scripts/Actor/Actor.cs
+++ Assets/Scripts/Actor/Actor.cs
@@ -14,16 +14,20 @@ public class Actor : MonoBehaviour
 
     public GameObject defaultBulletPrefab;
 
+    // 공격 목표
     private Transform target;
     public Transform GetTarget()
     {
         return target;
     }
 
-    protected Transform ShotPos;
+    public float shotPosDistance;
+
+    // 투사체 발사 위치
+    protected Transform shotPos;
     public Transform GetShotPos()
     {
-        return ShotPos;
+        return shotPos;
     }
     
     protected virtual void Awake()
@@ -33,7 +37,7 @@ public class Actor : MonoBehaviour
 
     protected virtual void Start()
     {
-        
+        shotPos = transform;
     }
 
     // Update is called once per frame
@@ -44,6 +48,8 @@ public class Actor : MonoBehaviour
 
     protected virtual void FixedUpdate()
     {
+        GameObject monsterObject = GameObject.Find("Monster");
+        SetShotPos(monsterObject.transform);
     }
 
     protected virtual void Attak()
@@ -56,5 +62,29 @@ public class Actor : MonoBehaviour
 
     protected virtual void Dead()
     {
+    }
+
+    protected virtual void SetShotPos(Transform _targetPos)
+    {
+        Vector3 direction = (_targetPos.position - transform.position).normalized;
+
+        // ShotPos가 Target을 향하도록 회전
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        shotPos.rotation = Quaternion.Euler(0, 0, angle);
+
+        // ShotPos 위치를 Player 기준으로 일정 거리 유지
+        shotPos.position = transform.position + direction * shotPosDistance;
+        shotPos.position = transform.position + direction * shotPosDistance;
+    }
+
+    void OnDrawGizmos()
+    {
+        if (shotPos != null)
+        {
+            // 그려질 색상 설정
+            Gizmos.color = Color.red;
+            // 2D 씬에서 targetTransform의 위치에 원 그리기
+            Gizmos.DrawWireSphere(shotPos.position, 0.02f);
+        }
     }
 }
