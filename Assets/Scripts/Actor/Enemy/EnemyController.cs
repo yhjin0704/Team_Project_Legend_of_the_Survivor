db826d48f9e666ff3feff084c1bf48ffd07708ef
yhjin0704 <yhjin07@naver.com>
2025-02-25 17:07:07 +0900

    <wip>

--- Assets/Scripts/Actor/Enemy/EnemyController.cs
+++ Assets/Scripts/Actor/Enemy/EnemyController.cs
@@ -26,37 +26,37 @@ public class EnemyController : BaseController
 
     protected void HandleAction()
     {
-        if (GetTarget() == null)
-        {
-            if (!movementDirection.Equals(Vector2.zero)) movementDirection = Vector2.zero;
-            return;
-        }
+        //if (GetTarget() == null)
+        //{
+        //    if (!movementDirection.Equals(Vector2.zero)) movementDirection = Vector2.zero;
+        //    return;
+        //}
 
-        float distance = DistanceToTarget();
-        Vector2 direction = DirectionToTarget();
+        //float distance = DistanceToTarget();
+        //Vector2 direction = DirectionToTarget();
 
-        isAttacking = false;
-        if (distance <= followRange)
-        {
-            lookDirection = direction;
+        //isAttacking = false;
+        //if (distance <= followRange)
+        //{
+        //    lookDirection = direction;
 
-            if (distance <= attackRange)
-            {
-                int layerMaskTarget = GetTarget().gameObject.layer;
-                RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, attackRange * 1.5f,
-                    (1 << LayerMask.NameToLayer("Level")) | layerMaskTarget);
+        //    if (distance <= attackRange)
+        //    {
+        //        int layerMaskTarget = GetTarget().gameObject.layer;
+        //        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, attackRange * 1.5f,
+        //            (1 << LayerMask.NameToLayer("Level")) | layerMaskTarget);
 
-                if (hit.collider != null && layerMaskTarget == (layerMaskTarget | (1 << hit.collider.gameObject.layer)))
-                {
-                    isAttacking = true;
-                }
+        //        if (hit.collider != null && layerMaskTarget == (layerMaskTarget | (1 << hit.collider.gameObject.layer)))
+        //        {
+        //            isAttacking = true;
+        //        }
 
-                movementDirection = Vector2.zero;
-                return;
-            }
+        //        movementDirection = Vector2.zero;
+        //        return;
+        //    }
 
-            movementDirection = direction;
-        }
+        //    movementDirection = direction;
+        //}
     }
 
     protected Vector2 DirectionToTarget()
