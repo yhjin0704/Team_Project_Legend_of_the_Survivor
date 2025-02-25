b7047cba9955a1153fce16c1c2549ec2365b966b
kimjinhong2023 <kimjinhong01@naver.com>
2025-02-25 20:48:46 +0900

    <Fix> 몬스터 애니메이션 그림자 수정

--- Assets/Scripts/Actor/Player/ClosestDistance.cs
+++ Assets/Scripts/Actor/Player/ClosestDistance.cs
@@ -3,36 +3,36 @@ using System.Collections.Generic;
 
 public class ClosestDistance : PlayerController
 {
-    public transform player; // �÷��̾� transform
-    public list<monster> listmonsters = new list<monster>(); // ���� ����Ʈ
-    private monster target;
+    //public transform player; // �÷��̾� transform
+    //public list<monster> listmonsters = new list<monster>(); // ���� ����Ʈ
+    //private monster target;
 
-    void updateclosestmonster()
-    {
-        if (listmonsters == null || listmonsters.count == 0)
-        {
-            target = null;
-            return;
-        }
+    //void updateclosestmonster()
+    //{
+    //    if (listmonsters == null || listmonsters.count == 0)
+    //    {
+    //        target = null;
+    //        return;
+    //    }
 
-        float mindistance = mathf.infinity;
-        monster closestmonster = null;
+    //    float mindistance = mathf.infinity;
+    //    monster closestmonster = null;
 
-        foreach (monster monster in listmonsters)
-        {
-            float distance = vector2.distance(player.position, monster.transform.position);
-            if (distance < mindistance)
-            {
-                mindistance = distance;
-                closestmonster = monster;
-            }
-        }
+    //    foreach (monster monster in listmonsters)
+    //    {
+    //        float distance = vector2.distance(player.position, monster.transform.position);
+    //        if (distance < mindistance)
+    //        {
+    //            mindistance = distance;
+    //            closestmonster = monster;
+    //        }
+    //    }
 
-        target = closestmonster;
-    }
+    //    target = closestmonster;
+    //}
 
     private void start()
     {
-        updateclosestmonster();
+        //updateclosestmonster();
     }
 }
