6881d64dae27e09cac6eb2965fc4dab098325a00
옙투 <dpqudrnjs1@naver.com>
2025-02-27 20:08:36 +0900

    <Feat>:Portal 작업?:

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -7,8 +7,10 @@ using UnityEngine.SceneManagement;
 public class MapManager : MonoBehaviour
 {
     public GameObject portalPrefab;      // ������ ��Ż ������
+    private GameObject portalInstance;
     private Collider2D portalCollider;
     private SpriteRenderer portalRenderer;
+    private bool isPortalActive = false;
     public GameObject potPrefab;        // �׾Ƹ� ������
     public Vector2[] potPositions;      // �ν����Ϳ��� ������ ��ǥ �迭
     public int currentMap;            // ���� ��
@@ -22,14 +24,41 @@ public class MapManager : MonoBehaviour
 
         SetPortalActive(false); // ���� �� ��Ż ��Ȱ��ȭ
     }
-
-    private void OnTriggerEnter2D(Collider2D other)
+    void Update()
     {
-        if (other.CompareTag("Player"))
+        if (AllEnemyDefeat())
         {
-            LoadNextRoom();
+            if (!isPortalActive)
+            {
+                SpawnPortal();
+                isPortalActive = true;
+            }
+        }
+        else
+        {
+            if (portalInstance != null)
+            {
+                Destroy(portalInstance);
+                isPortalActive = false;
+            }
         }
     }
+
+    // ��� ���� �׾����� Ȯ���ϴ� �Լ�
+    private bool AllEnemyDefeat()
+    {
+        return GameObject.FindGameObjectsWithTag("Enemy").Length == 0;
+    }
+
+    // ��Ż ���� �Լ�
+    private void SpawnPortal()
+    {
+        Vector2 spawnPosition = new Vector2(-9, -4);                                                // �ٴڿ� ��Ż ���� (��ġ ���� ����)
+        GameObject portalObject = Instantiate(portalPrefab, spawnPosition, Quaternion.identity);
+        portalObject.GetComponent<SpawnManager>();
+        SetPortalActive(true);
+    }
+    
 
     // ��Ż Ȱ��ȭ/��Ȱ��ȭ �Լ�
     public void SetPortalActive(bool isActive)
@@ -38,12 +67,6 @@ public class MapManager : MonoBehaviour
         portalRenderer.enabled = isActive;
     }
 
-    // ���� ��(��) �ε�
-    private void LoadNextRoom()
-    {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentSceneIndex + 1);
-    }
 
 
     void Start()
@@ -57,32 +80,5 @@ public class MapManager : MonoBehaviour
         {
             Instantiate(potPrefab, position, Quaternion.identity);
         }
-
-
-
-        void Update()
-        {
-            if (AllEnemiesDefeated() == null)
-            {
-                SpawnPortal();
-            }
-        }
     }
-
-    // ��� ���� �׾����� Ȯ���ϴ� �Լ�
-    private bool AllEnemiesDefeated()
-    {
-        return GameObject.FindGameObjectsWithTag("Enemy").Length == 0;
-    }
-
-    // ��Ż ���� �Լ�
-    private void SpawnPortal()
-    {
-        Vector2 spawnPosition = new Vector2(-9, -4);                                                // �ٴڿ� ��Ż ���� (��ġ ���� ����)
-        GameObject portalObject = Instantiate(portalPrefab, spawnPosition, Quaternion.identity);
-        portalObject.GetComponent<SpawnManager>();
-        SetPortalActive(true); // ��Ż Ȱ��ȭ
-    }
-
-
 }
