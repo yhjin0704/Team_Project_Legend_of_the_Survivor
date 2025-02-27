32997076f8befffa6a59acba7a3b8fc6d19b2067
yhjin0704 <yhjin07@naver.com>
2025-02-27 11:05:47 +0900

    <feat>: Potion 추가

--- Assets/Scripts/Actor/Player/TakeItemRange.cs
+++ Assets/Scripts/Actor/Player/TakeItemRange.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TakeGoldRange : MonoBehaviour
+public class TakeItemRange : MonoBehaviour
 {
     Player player;
+    PlayerController playerController;
     private Collider2D col;
 
     public float Range = 1;
@@ -12,6 +13,8 @@ public class TakeGoldRange : MonoBehaviour
     private void Awake()
     {
         player = transform.parent.GetComponent<Player>();
+        playerController = transform.parent.GetComponent<PlayerController>();
+
         col = GetComponent<Collider2D>();
 
         col.transform.localScale = new Vector2(Range, Range);
@@ -19,17 +22,5 @@ public class TakeGoldRange : MonoBehaviour
 
     private void Start()
     {
-    }
-
-    private void OnTriggerEnter2D(Collider2D _collision)
-    {
-        if (_collision.CompareTag("Coin"))
-        {
-            player.gold += 1;
-
-            _collision.gameObject.GetComponent<Collider2D>().enabled = false;
-
-            _collision.gameObject.GetComponent<Animator>().Play("Coin_Destroy");
-        }
     }
 }
